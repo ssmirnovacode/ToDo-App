66a5579032758bd4d6a5a68db5ce6a6d002174e0
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-04 14:11:58 +0100

    Bootswatch theme sketchy enabled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import 'bootswatch/dist/sketchy/bootstrap.min.css';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
