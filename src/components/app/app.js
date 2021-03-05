d51e29c25bd18e531b7b1bc83c60219c0c87e277
ssmirnova <svetlana.smirnova213@gmail.com>
2021-03-05 19:08:46 +0100

    login enabled withput password

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,7 +11,7 @@ import UsernameForm from '../username/username';
 
 const App = () => {
 
-    const path = baseURL + 'users/' + localStorage.getItem('user') + '/';
+    const user = localStorage.getItem('user') || '';
 
     const reqService = new RequestService();
 
@@ -20,15 +20,17 @@ const App = () => {
             label,
             important: false,
             done: false,
-            id: Math.random()
+            id: Math.random(),
+            owner: localStorage.getItem('user')
         }
     };
 
     const [items, setItems] = useState([]);
      
     useEffect( () => {
+        localStorage.clear();
         let mounted = true;
-        reqService.getItems(path + 'items')
+        reqService.getItems(baseURL + 'items')
         .then( res => {mounted && setItems(res)})
         .catch(() => console.log('GET error!'));
         return () => mounted = false;
@@ -38,9 +40,11 @@ const App = () => {
 
     const [filter, setFilter] = useState('all');
 
+    const [loggedIn, setLoggedIn] = useState(false);
+
     const deleteItem = id => { 
         if (window.confirm('Are you sure you want to delete this item?')) {
-            reqService.deleteItem(path + 'items/'+ id)
+            reqService.deleteItem(baseURL + 'items/' + id)
             .then(() => {
                 console.log(`Item deleted`); 
                 const idx = items.findIndex( el => el.id === id);
@@ -54,7 +58,7 @@ const App = () => {
 
     const addItem = (label) => { 
         const newItem = createNewItem(label);
-        reqService.postItem(path + 'items', newItem)
+        reqService.postItem(baseURL + 'items', newItem)
         .then(() => setItems([...items, newItem]))
         .catch(() => console.log('POST error'));
     };
@@ -64,7 +68,7 @@ const App = () => {
             const oldItem = array[idx];
             const updatedItem = {...oldItem, [statusName]: !oldItem[statusName]}; // superficial copy of oldItem and updated property
             
-            reqService.updateItem(path + 'items/'+ id, updatedItem)
+            reqService.updateItem(baseURL + 'items/' + id, updatedItem)
             .then(() => setItems([
                 ...array.slice(0, idx),
                 updatedItem,
@@ -91,12 +95,11 @@ const App = () => {
 
     const handleLogin = (username) => {
         localStorage.setItem('user', username);
-
+        setLoggedIn(true);
     }
 
-    const doneCount = items.filter(el => el.done === true).length;
-    const pendingCount = items.length - doneCount;
-    const visibleItems = items.filter(item => item.label.toLowerCase().indexOf(pattern.toLowerCase()) > -1)
+    const visibleItems = items.filter(item => item.owner === user)
+                                .filter(item => item.label.toLowerCase().indexOf(pattern.toLowerCase()) > -1)
                                 .filter(item => {
                                     if (filter === 'done') {
                                         return item.done === true
@@ -106,10 +109,12 @@ const App = () => {
                                     }
                                     else return item
                                 });
-    
-    return(
-        <div className="todo-app">
-            <UsernameForm onLogin={handleLogin}/>
+
+    const doneCount = visibleItems.filter(el => el.done === true).length;
+    const pendingCount = visibleItems.length - doneCount;
+
+    const list = loggedIn ? 
+        <>
             <AppHeader toDo={pendingCount} done={doneCount} />
             <div className="top-panel d-flex">
                 <SearchPanel value={pattern} onSearch={searchItems}/>
@@ -117,7 +122,18 @@ const App = () => {
             </div>
             <TodoList items={visibleItems} onDelete={deleteItem} 
                 onToggleDone={toggleDone} onToggleImportant={toggleImportant}/>
-            <ItemAddForm onAdd={addItem}/>
+            <ItemAddForm onAdd={addItem}/> 
+        </> : <div>Please log in</div>
+    const userPanel = loggedIn ? 
+        <>
+            <div className="greeting mr-2">Hello, {user}</div> 
+            <button className="btn btn-outline-secondary" onClick={() => setLoggedIn(false)}>Log out</button>
+        </>
+        : <UsernameForm onLogin={handleLogin}/>
+    return(
+        <div className="todo-app">
+            {userPanel}
+            {list}
         </div>
     )
     
