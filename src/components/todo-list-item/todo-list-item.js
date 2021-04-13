7a9ab15f6f03523c545b3ac7748be217754f15ea
ssmirnova <svetlana.smirnova213@gmail.com>
2021-04-13 18:25:31 +0200

    Lines made adaptive with d-flex

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -17,24 +17,26 @@ const TodoListItem = (props) => {
     const btnClass = important ? "btn btn-outline-danger btn-sm float-right mr-2 active" : "btn btn-outline-danger btn-sm float-right mr-2";
   
     return (
-      <div className={itemClass}>
-        <span
+      <div className={itemClass} data-todoitem>
+        <div
           className="todo-list-item-label"
           onClick={onToggleDone}>
           {label}
-        </span>
+        </div>
 
-        <button type="button"
-                className="btn btn-outline-secondary btn-sm float-right"
-                onClick={onDelete}>
-          <i className="fa fa-trash-o" />
-        </button>
-  
-        <button type="button"
-                className={btnClass}
-                onClick={onToggleImportant} >
-          <i className="fa fa-exclamation" />
-        </button>
+        <div className="todo-btn-group">
+          <button type="button"
+                  className={darkmode ? btnClass+' darkmode' : btnClass}
+                  onClick={onToggleImportant} >
+            <i className="fa fa-exclamation" />
+          </button>
+          <button type="button"
+                  className="btn btn-outline-secondary btn-sm float-right"
+                  onClick={onDelete}>
+            <i className="fa fa-trash-o" />
+          </button>
+          
+        </div>
       
       </div>
     );  
